e61eb3444379f8fd447ba61798dbf6672d73346f
Nick Pfisterer <nick@polymoon.net>
2020-01-11 14:38:20 -0800

    Update bot config
    - Added embed colors
    - Add comments explaining each property
    - Added whitespace between properties

--- quin.config.js
+++ quin.config.js
@@ -1,4 +1,17 @@
 export default {
+  // Used as the prefix for commands, e.g. !ping
   commandPrefix: '!',
-  serverDescription: ''
+
+  // Appears under the server name when the Server Info command is used
+  serverDescription: '',
+
+  // Can be used to color the left border on embeds
+  embedColors: {
+    red: [255, 107, 107],
+    orange: [255, 169, 77],
+    yellow: [255, 224, 102],
+    green: [192, 235, 117],
+    blue: [77, 171, 247],
+    purple: [229, 153, 247]
+  }
 }
