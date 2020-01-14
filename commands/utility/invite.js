a1105640e0ba754b8164ee9a4cae881d4f2b6537
Nick Pfisterer <nick@polymoon.net>
2020-01-13 20:01:33 -0800

    Add command: Invite

---
+++ commands/utility/invite.js
@@ -0,0 +1,23 @@
+import { Command } from 'discord-akairo'
+
+class InviteCommand extends Command {
+  constructor () {
+    super('invite', {
+      aliases: ['invite'],
+      category: 'Utility',
+      description: {
+        content: 'Get the invite link for this server',
+        usage: '!invite'
+      },
+      channelRestriction: 'guild',
+      userPermissions: ['SEND_MESSAGES'],
+    })
+  }
+
+  exec (message) {
+    // Reply with invite link
+    return message.util.send(`https://discord.gg/${message.guild.fetchVanityCode()}`)
+  }
+}
+
+export default InviteCommand
