d45e5d84e7bad0a80ea23bf7ad9d5bfd9a363b28
Nick Pfisterer <nick@polymoon.net>
2020-01-14 19:21:50 -0800

    Add listeners: Command Enabled, Command Disabled
    These are used to log when bot commands are enabled or disabled.

---
+++ listeners/commandHandler/commandDisabled.js
@@ -0,0 +1,24 @@
+import { Listener } from 'discord-akairo'
+import log from '../../util/logger.js'
+
+class CommandDisabledListener extends Listener {
+  constructor () {
+    super('commandDisabled', {
+      emitter: 'commandHandler',
+      eventName: 'disable'
+    })
+  }
+
+  async exec (command) {
+    // Get the server log channel
+    const channel = await this.client.channels.find(c => c.name === this.client.config.modLogChannel)
+
+    // Log for debugging
+    log.info(`The ${command.id} command was disabled.`)
+
+    // Send the message
+    return channel.send(`:no_entry_sign: The **${command.id}** command was disabled.`)
+  }
+}
+
+export default CommandDisabledListener
