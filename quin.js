b9b049928190a2b543d33a8fb800b4b9eb162937
Nick Pfisterer <nick@polymoon.net>
2020-03-04 21:28:35 -0800

    Refactor client

--- quin.js
+++ quin.js
@@ -1,11 +1,11 @@
-import QuinClient from './struct/QuinClient'
+import { AkairoClient, CommandHandler, ListenerHandler } from 'discord-akairo'
 import cli from 'commander'
 import config from './quin.config.js'
 import pkg from './package.json'
 import * as Sentry from '@sentry/node'
 
-cli
-  .version(pkg.version)
+// CLI
+cli.version(pkg.version)
   .option('-t, --token <token>', 'Bot token')
   .option('-o, --owner <id>', 'Owner ID')
   .option('-s, --sentry <dsn>', 'Sentry DSN')
@@ -15,8 +15,58 @@ const token = cli.token ? cli.token : process.env.BOT_TOKEN
 const owner = cli.owner ? cli.owner : process.env.OWNER_ID
 const dsn = cli.sentry ? cli.sentry : process.env.SENTRY_DSN
 
-const client = new QuinClient(config, owner)
-
+// Sentry
 Sentry.init({ dsn: dsn })
+
+// Discord client
+class QuinClient extends AkairoClient {
+  constructor () {
+    super({
+      // Akairo options
+      ownerId: owner
+    }, {
+      // Discord.js options
+      disableMentions: 'everyone',
+      ws: {
+        intents: [
+          'GUILDS',
+          'GUILD_MEMBERS',
+          'GUILD_BANS',
+          'GUILD_EMOJIS',
+          'GUILD_VOICE_STATES',
+          'GUILD_PRESENCES',
+          'GUILD_MESSAGES',
+          'GUILD_MESSAGE_REACTIONS',
+          'DIRECT_MESSAGES',
+          'DIRECT_MESSAGE_REACTIONS'
+        ]
+      }
+    })
+
+    // Config
+    this.config = config
+
+    // Commands
+    this.commandHandler = new CommandHandler(this, {
+      directory: './commands/',
+      allowMention: true,
+      commandUtil: true,
+      handleEdits: true,
+      prefix: config.commandPrefix
+    })
+
+    this.commandHandler.loadAll()
+
+    // Listeners
+    this.listenerHandler = new ListenerHandler(this, {
+      directory: './listeners/'
+    })
+
+    this.listenerHandler.loadAll()
+    this.commandHandler.useListenerHandler(this.listenerHandler)
+  }
+}
+
+const client = new QuinClient()
 
 client.login(token)
