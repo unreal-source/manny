945e051945a08550c5edd5a25d077d1523691dac
Nick Pfisterer <nick@polymoon.net>
2020-03-12 21:32:14 -0700

    Update Server Info command

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -16,30 +16,24 @@ class ServerInfoCommand extends Command {
     })
   }
 
-  exec (message) {
-    // Server object
-    const guild = message.guild
-
-    // Human-friendly server creation date
+  async exec (message) {
+    const guild = await message.guild.fetch()
     const creationDate = DateTime.fromISO(guild.createdAt.toISOString())
+    const totalMembers = guild.memberCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    const onlineMembers = guild.members.cache.filter(member => member.presence.status === 'online').size.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 
-    // Total members
-    const totalMembers = guild.memberCount
-
-    // Online members
-    const onlineMembers = guild.members.cache.filter(member => member.presence.status === 'online').size
-
-    // Initialize & populate embed
     const embed = this.client.util.embed()
       .setColor(config.embedColors.violet)
       .setTitle(guild.name)
-      .setDescription(config.serverDescription)
+      .setDescription(guild.description ? message.guild.description : '')
       .setThumbnail(guild.iconURL())
+      .addField('Members', totalMembers, true)
+      .addField('Online', onlineMembers, true)
       .addField('Created On', `${creationDate.toLocaleString(DateTime.DATE_SHORT)} ${creationDate.toLocaleString(DateTime.TIME_SIMPLE)} ${creationDate.offsetNameShort}`)
-      .addField('Members', `${totalMembers} (${onlineMembers} online)`)
-      .addField('Invite', `https://discord.gg/${guild.vanityURLCode}`)
+      .addField('Website', config.links.website)
+      .addField('GitHub', config.links.github)
+      .addField('Twitter', config.links.twitter)
 
-    // Send the embed
     return message.util.send({ embed })
   }
 }
