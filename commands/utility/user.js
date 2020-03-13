2c25880f8446b49ab5f9a10d3082661797112016
Nick Pfisterer <nick@polymoon.net>
2020-03-12 19:52:44 -0700

    Refactor User Info command

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -31,35 +31,30 @@ class UserInfoCommand extends Command {
   }
 
   async exec (message, { member }) {
-    // Human-friendly date when this user joined Discord
     const discordJoinDate = DateTime.fromISO(member.user.createdAt.toISOString())
 
-    // Human-friendly date when this user joined the guild
     const guildJoinDate = DateTime.fromISO(member.joinedAt.toISOString())
 
-    // Human-friendly date when this user's last message was sent
     const lastMessageDate = DateTime.fromISO(member.lastMessage.createdAt.toISOString())
 
-    // Human-friendly statuses
     const status = {
-      online: 'Online',
-      idle: 'Idle',
-      dnd: 'Do Not Disturb',
-      offline: 'Offline'
+      online: ':green_circle: Online',
+      idle: ':orange_circle: Idle',
+      dnd: ':red_circle: Do Not Disturb',
+      offline: ':black_circle: Offline'
     }
 
-    // Initialize & populate embed
     const embed = this.client.util.embed()
       .setColor(config.embedColors.violet)
       .setThumbnail(member.user.displayAvatarURL())
-      .setTitle(member.user.bot ? `${member.displayName} (${status[member.presence.status]}) :robot:` : `${member.displayName} (${status[member.presence.status]})`)
+      .setTitle(member.user.bot ? `${member.displayName} :robot:` : `${member.displayName}`)
+      .setDescription(status[member.presence.status])
       .addField('Username', member.user.tag)
       .addField('ID', member.id)
       .addField('Joined Server', `${guildJoinDate.toLocaleString(DateTime.DATE_SHORT)} ${guildJoinDate.toLocaleString(DateTime.TIME_SIMPLE)} ${guildJoinDate.offsetNameShort}`)
       .addField('Joined Discord', `${discordJoinDate.toLocaleString(DateTime.DATE_SHORT)} ${discordJoinDate.toLocaleString(DateTime.TIME_SIMPLE)} ${discordJoinDate.offsetNameShort}`)
       .addField('Last Seen', `${lastMessageDate.toLocaleString(DateTime.DATE_SHORT)} ${lastMessageDate.toLocaleString(DateTime.TIME_SIMPLE)} ${lastMessageDate.offsetNameShort}`)
 
-    // Send embed
     return message.util.send({ embed })
   }
 }
