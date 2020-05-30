51958ff0ca35b16b853731c37d9df497f1e4023a
Nick Pfisterer <nick@polymoon.net>
2020-05-29 20:19:20 -0700

    Add command: Infraction History
    - Allows moderators to check a user's infraction history
    - Fetches history from the database if it exists and displays it in a rich embed
    - Notifies moderator if no history was found

---
+++ commands/moderation/history.js
@@ -0,0 +1,57 @@
+import { Command } from 'discord-akairo'
+import config from '../../bot.config'
+import { DateTime } from 'luxon'
+import InfractionHistory from '../../models/Infractions'
+import log from '../../utilities/logger'
+
+class UserHistoryCommand extends Command {
+  constructor () {
+    super('history', {
+      aliases: ['history'],
+      category: 'Moderation',
+      description: {
+        name: 'User History',
+        content: 'Check a user\'s infraction history.',
+        usage: '`!history <user>'
+      },
+      channel: 'guild',
+      memberPermissions: ['BAN_MEMBERS']
+    })
+  }
+
+  * args () {
+    const user = yield {
+      type: 'user',
+      prompt: {
+        start: 'Who\'s infraction history do you want to check?',
+        retry: 'User not found. Please enter a valid @mention or ID.'
+      }
+    }
+
+    return { user }
+  }
+
+  async exec (message, { user }) {
+    const history = await InfractionHistory.findOne({
+      where: { user_id: user.id }
+    })
+
+    if (history) {
+      log.debug(history.bans)
+      const embed = this.client.util.embed()
+        .setColor(config.embedColors.violet)
+        .setThumbnail(user.displayAvatarURL())
+        .setTitle(user.username)
+        .setDescription(user.bot ? `${user.tag} \`BOT\`` : user.tag)
+        .addField('Mutes', 'None')
+        .addField('Strikes', 'None')
+        .addField('Bans', history.bans.length === 0 ? 'None' : history.bans.map(entry => `**${DateTime.fromISO(entry.date).toLocaleString(DateTime.DATETIME_FULL)}**\n> Banned by \`${entry.executor}\`\n> Reason: ${entry.reason}`).join('\n'))
+
+      return message.channel.send({ embed })
+    }
+
+    return message.channel.send(`**${user.tag}** has no infraction history.`)
+  }
+}
+
+export default UserHistoryCommand
