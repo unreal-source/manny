4779006d37ae72de90e906be1ba255b15dfa2d52
Nick Pfisterer <nick@polymoon.net>
2020-08-28 22:19:31 -0700

    Add command: Lock Channel

---
+++ commands/moderator/lock.js
@@ -0,0 +1,54 @@
+import { Command } from 'discord-akairo'
+import log from '../../utilities/logger'
+
+class LockChannelCommand extends Command {
+  constructor () {
+    super('lock', {
+      aliases: ['lock'],
+      category: 'Moderator',
+      description: {
+        name: 'Lock Channel',
+        content: 'Make a channel read-only for regular members',
+        usage: '!lock [channel]'
+      },
+      channel: 'guild',
+      userPermissions: ['BAN_MEMBERS']
+    })
+  }
+
+  * args () {
+    const channel = yield {
+      type: 'channel',
+      default: message => message.channel,
+      prompt: {
+        start: 'Which channel do you want to lock?',
+        retry: 'Channel not found. Please enter a valid channel name, mention, or ID.',
+        optional: true
+      }
+    }
+
+    return { channel }
+  }
+
+  async exec (message, { channel }) {
+    if (channel.permissionOverwrites.get(channel.guild.id).deny.has('VIEW_CHANNEL')) {
+      return message.util.send(`${channel} is unaffected by locks because it's restricted.`)
+    }
+
+    if (!channel.permissionOverwrites.get(channel.guild.id).deny.has('SEND_MESSAGES')) {
+      await channel.updateOverwrite(channel.guild.roles.everyone, {
+        SEND_MESSAGES: false
+      })
+
+      if (message.channel !== channel) {
+        message.util.send(`:lock: ${channel} is now locked.`)
+      }
+
+      return channel.send(':lock: **Channel locked**')
+    } else {
+      return message.util.send(`${message.channel === channel ? 'This channel' : channel} is already locked.`)
+    }
+  }
+}
+
+export default LockChannelCommand
