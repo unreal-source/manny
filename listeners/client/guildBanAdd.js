cda43c5fdc79aae549f53c116b43bf6c890c91a3
Nick Pfisterer <nick@polymoon.net>
2020-09-25 16:58:32 -0700

    Update guildBanAdd listener
    - Use new data model
    - Update mod log format
    - Add try/catch block for better error reporting
    
    Unfortunately receipts aren't supported here. The user is already banned when this event triggers, so the bot can't send them a DM.

--- listeners/client/guildBanAdd.js
+++ listeners/client/guildBanAdd.js
@@ -1,6 +1,7 @@
 import { Listener } from 'discord-akairo'
 import config from '../../config'
-import InfractionHistory from '../../models/Infractions'
+import Case from '../../models/cases'
+import formatDate from '../../utilities/formatDate'
 
 class GuildBanAddListener extends Listener {
   constructor () {
@@ -11,46 +12,40 @@ class GuildBanAddListener extends Listener {
   }
 
   async exec (guild, user) {
-    const channel = this.client.channels.cache.get(config.logs.modLog)
-    const auditLogs = await guild.fetchAuditLogs({ type: 'MEMBER_BAN_ADD' })
-    const entry = auditLogs.entries.first()
+    try {
+      const auditLogs = await guild.fetchAuditLogs({ type: 'MEMBER_BAN_ADD' })
+      const entry = auditLogs.entries.first()
 
-    // If the user was banned with a bot command, logging via this event will show the bot as the executor.
-    // Since we always want the executor to be the moderator who took the action, we skip logging here
-    // and let the command handle it.
-    if (entry.executor.id === this.client.user.id) {
-      return
-    }
+      // If the user was banned with a bot command, logging via this event will show the bot as the executor.
+      // Since we always want the executor to be the moderator who took the action, we skip logging here
+      // and let the command handle it.
+      if (entry.executor.id === this.client.user.id) {
+        return
+      }
 
-    const ban = {
-      action: 'ban',
-      date: entry.createdAt,
-      executor: entry.executor.tag,
-      reason: entry.reason
-    }
-
-    const history = await InfractionHistory.findOne({
-      where: { user_id: user.id }
-    })
-
-    if (history) {
-      const bans = history.bans
-      bans.push(ban)
-      await InfractionHistory.update({
-        bans: bans
-      }, {
-        where: { user_id: user.id }
+      // Record case
+      await Case.create({
+        action: 'ban',
+        user: user.id,
+        moderator: entry.executor.id,
+        reason: entry.reason,
+        timestamp: entry.createdAt
       })
-    } else {
-      await InfractionHistory.create({
-        user_id: user.id,
-        mutes: [],
-        strikes: [],
-        bans: [ban]
-      })
-    }
 
-    return channel.send(`:no_entry_sign: **${user.tag}** was banned by **${ban.executor}**. Reason: ${ban.reason}`)
+      // Send mod log
+      const logChannel = this.client.channels.cache.get(config.logs.channels.modLog)
+      const logEntry = this.client.util.embed()
+        .setColor(config.embeds.colors.red)
+        .setAuthor(entry.executor.tag, entry.executor.displayAvatarURL())
+        .setThumbnail(user.displayAvatarURL())
+        .setTitle(`${config.prefixes.ban} Banned ${user.tag}`)
+        .setDescription(`**Reason:** ${entry.reason}`)
+        .setFooter(formatDate(entry.createdAt))
+
+      return logChannel.send({ embed: logEntry })
+    } catch (err) {
+      this.client.log.error(err)
+    }
   }
 }
 
