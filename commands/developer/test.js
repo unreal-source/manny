f264abe23082eb3a67d6541461d769c1ebff20bd
Nick Pfisterer <nick@polymoon.net>
2020-09-24 17:47:16 -0700

    Update config references

--- commands/developer/test.js
+++ commands/developer/test.js
@@ -14,7 +14,7 @@ class TestCommand extends Command {
       category: 'Developer',
       description: {
         name: 'Test',
-        content: 'A private command for testing embed designs and other things.',
+        content: 'Debug command for testing embed designs.',
         usage: '!test'
       },
       channel: 'guild',
@@ -35,49 +35,49 @@ class TestCommand extends Command {
     switch (design) {
       case 'ban':
         embed
-          .setColor(config.embedColors.red)
-          .setAuthor(`${message.author.username} • ${message.author.id}`, message.author.displayAvatarURL())
-          .setTitle(`${config.emoji.ban} Banned by Cedric 'eXi' Neukirchen#4538`)
+          .setColor(config.embeds.colors.red)
+          .setAuthor(`${message.author.tag} • ${message.author.id}`, message.author.displayAvatarURL())
+          .setTitle(`${config.prefixes.ban} Banned by Cedric 'eXi' Neukirchen#4538`)
           .setDescription('Reason: Being a terrible person')
           .setFooter(timestamp)
         break
       case 'unban':
         embed
-          .setColor(config.embedColors.red)
+          .setColor(config.embeds.colors.red)
           .setTitle(':arrow_right_hook: __motherflanker#6666__ was unbanned by __Cedric \'eXi\' Neukirchen#4538__')
           .setDescription('Reason: Learned from their mistakes')
           .setFooter(timestamp)
         break
       case 'mute':
         embed
-          .setColor(config.embedColors.yellow)
+          .setColor(config.embeds.colors.yellow)
           .setTitle(':clock2: __motherflanker#6666__ was muted for __1h__ by __Cedric \'eXi\' Neukirchen#4538__')
           .setDescription('Reason: Being a jerk and disrupting conversations')
           .setFooter(timestamp)
         break
       case 'unmute':
         embed
-          .setColor(config.embedColors.yellow)
+          .setColor(config.embeds.colors.yellow)
           .setTitle(':arrow_right_hook: __motherflanker#6666__ was unmuted by __Cedric \'eXi\' Neukirchen#4538__')
           .setDescription('Reason: Muted the wrong person')
           .setFooter(timestamp)
         break
       case 'expired':
         embed
-          .setColor(config.embedColors.yellow)
+          .setColor(config.embeds.colors.yellow)
           .setTitle(':alarm_clock: Mute expired on __motherflanker#6666__')
           .setFooter(timestamp)
         break
       case 'strike':
         embed
-          .setColor(config.embedColors.orange)
+          .setColor(config.embeds.colors.orange)
           .setTitle(':triangular_flag_on_post: __motherflanker#6666__ received a strike from __Cedric \'eXi\' Neukirchen#4538__')
           .setDescription('Reason: Spamming Discord invite links')
           .setFooter(timestamp)
         break
       case 'pardon':
         embed
-          .setColor(config.embedColors.orange)
+          .setColor(config.embeds.colors.orange)
           .setTitle(':arrow_right_hook: __motherflanker#6666__ had 1 strike removed by __Cedric \'eXi\' Neukirchen#4538__')
           .setDescription('Reason: Previous strike was a mistake')
           .setFooter(timestamp)
@@ -93,35 +93,36 @@ class TestCommand extends Command {
         break
       case 'join':
         embed
-          .setColor(config.embedColors.green)
+          .setColor(config.embeds.colors.green)
           .setThumbnail(message.author.displayAvatarURL())
           .setTitle(':inbox_tray: __motherflanker#6666__ joined the server')
           .setFooter(timestamp)
         break
       case 'bot':
         embed
-          .setColor(config.embedColors.blue)
+          .setColor(config.embeds.colors.blue)
           .setTitle(':robot: __Manny#4953__ was added to the server')
           .setDescription('<@435533673484386306>')
           .setFooter(timestamp)
         break
       case 'leave':
         embed
-          .setColor(config.embedColors.red)
+          .setColor(config.embeds.colors.red)
           .setTitle(':outbox_tray: __motherflanker#6666__ left the server')
           .setFooter(timestamp)
         break
       case 'ban-2':
         embed
-          .setColor(config.embedColors.red)
-          .setTitle(':no_entry_sign: motherflanker#6666 was banned')
+          .setColor(config.embeds.colors.red)
+          .setAuthor(message.author.tag, message.author.displayAvatarURL())
+          .setTitle(`${config.prefixes.ban} User Banned`)
           .addField('Moderator', 'Cedric \'eXi\' Neukirchen#4538')
           .addField('Reason', 'Being a terrible person')
           .setFooter(timestamp)
         break
       case 'strike-2':
         embed
-          .setColor(config.embedColors.orange)
+          .setColor(config.embeds.colors.orange)
           .setTitle(':triangular_flag_on_post: !₮ØØⱧł₲Ⱨ₮Ø₵Ø₥₱ⱠɎ𝕯𝖎𝖘𝖈𝖔𝖗𝖉𝕳𝖎𝖘𝖙𝖔𝖗𝖎𝖆𝖓#7788 was given their first strike')
           .addField('Moderator', 'Cedric \'eXi\' Neukirchen#4538')
           .addField('Reason', 'Being a terrible person')
