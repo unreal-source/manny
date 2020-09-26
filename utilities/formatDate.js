94bb106c3f427532a950df48c756a2ebe664b024
Nick Pfisterer <nick@polymoon.net>
2020-09-25 22:37:45 -0700

    Support Postgres timestamps

--- utilities/formatDate.js
+++ utilities/formatDate.js
@@ -1,5 +1,9 @@
 import { DateTime } from 'luxon'
 
-export default function formatDate (date) {
+export default function formatDate (date, sql = false) {
+  if (sql) {
+    return DateTime.fromObject(date).toLocaleString(DateTime.DATETIME_FULL)
+  }
+
   return DateTime.fromISO(date.toISO()).toLocaleString(DateTime.DATETIME_FULL)
 }
