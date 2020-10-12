883b95f01e97edc833d58a8b83ec39e9e12b154c
Nick Pfisterer <nick@polymoon.net>
2020-10-11 20:30:25 -0700

    Update Unmute command to use new mute system

--- commands/moderator/unmute.js
+++ commands/moderator/unmute.js
@@ -1,8 +1,8 @@
 import { Command } from 'discord-akairo'
 import { DateTime } from 'luxon'
 import config from '../../config'
-import formatDate from '../../utilities/formatDate'
-import InfractionHistory from '../../models/Infractions'
+import Case from '../../models/cases'
+import Mute from '../../models/mutes'
 
 class UnmuteCommand extends Command {
   constructor () {
@@ -41,62 +41,61 @@ class UnmuteCommand extends Command {
 
   async exec (message, { member, reason }) {
     if (member.id === message.author.id) {
-      return message.channel.send(`${config.emoji.warning} You cannot unmute yourself.`)
+      return message.channel.send(`${config.emoji.warning} You can't unmute yourself.`)
     }
 
     if (member.id === this.client.user.id) {
-      return message.channel.send(`${config.emoji.warning} You cannot unmute me.`)
+      return message.channel.send(`${config.emoji.warning} Nice try, human.`)
     }
 
     if (!member.roles.cache.some(role => role.name === 'Muted')) {
       return message.channel.send(`${config.emoji.warning} That user is not muted.`)
     }
 
-    const role = await message.guild.roles.cache.find(role => role.name === 'Muted')
-    const unmuted = await member.roles.remove(role)
+    const mutedRole = await message.guild.roles.fetch(config.infractions.mutedRole)
 
-    clearTimeout(this.client.mutes.get(member.id))
-    this.client.mutes.delete(member.id)
+    // Take action
+    await member.roles.remove(mutedRole)
 
-    message.channel.send(`You unmuted ${unmuted}.`)
-
-    const now = DateTime.local().toISO()
-    const unmute = {
+    // Record case
+    const record = await Case.create({
       action: 'unmute',
-      date: now,
-      executor: message.author.tag,
-      reason: reason
-    }
-
-    const history = await InfractionHistory.findOne({
-      where: { user_id: member.id }
+      user: member.user.tag,
+      moderator: message.author.tag,
+      reason: reason,
+      timestamp: DateTime.local()
     })
 
-    if (history) {
-      const mutes = history.mutes
-      mutes.push(unmute)
-      await InfractionHistory.update({
-        mutes: mutes
-      }, {
-        where: { user_id: member.id }
-      })
-    } else {
-      await InfractionHistory.create({
-        user_id: member.id,
-        mutes: [unmute],
-        strikes: [],
-        bans: []
-      })
-    }
+    // Remove from mute schedule
+    await Mute.destroy({
+      where: {
+        id: member.id
+      }
+    })
 
-    const logChannel = this.client.channels.cache.get(config.logs.modLog)
-    const embed = this.client.util.embed()
-      .setColor(config.embedColors.yellow)
-      .setTitle(`${config.emoji.undo} __${member.user.tag}__ was unmuted by __${message.author.tag}__`)
-      .setDescription(`Reason: ${reason}`)
-      .setFooter(formatDate(now))
+    // Send mod log
+    const logChannel = this.client.channels.cache.get(config.logs.channels.modLog)
+    const logEntry = this.client.util.embed()
+      .setColor(config.embeds.colors.yellow)
+      .setAuthor(member.user.tag, member.user.displayAvatarURL())
+      .setTitle(`${config.prefixes.undo} Member unmuted`)
+      .setDescription(`by ${message.author.tag}`)
+      .addField('Reason', reason)
+      .setFooter(`#${record.id}`)
+      .setTimestamp()
 
-    return logChannel.send({ embed })
+    await logChannel.send({ embed: logEntry })
+
+    // Send receipt
+    const receipt = this.client.util.embed()
+      .setColor(config.embeds.colors.yellow)
+      .setAuthor(message.guild.name, message.guild.iconURL())
+      .setTitle(`${config.prefixes.undo} You were unmuted`)
+      .addField('Reason', reason)
+      .setFooter(`#${record.id}`)
+      .setTimestamp()
+
+    return member.send({ embed: receipt })
   }
 }
 
