ede62c44d665c7997a0ae637276d4614b5cb3328
Nick Pfisterer <nick@polymoon.net>
2020-11-02 20:01:27 -0800

    Add game idea templates

--- commands/fun/gameidea.js
+++ commands/fun/gameidea.js
@@ -18,8 +18,30 @@ class GameIdeaCommand extends Command {
 
   exec (message) {
     const templates = [
-      'A {style:a} {genre:a} where you {verb} {supernatural:plural} with {object:plural} in {setting:in}',
-      'A {style:a} {genre:a} where {person:plural} take on a horde of {supernatural:plural}, with a twist: {diversifier}'
+      'A game where you {verb} {object:plural} to {goal}.',
+      'A game where you {verb} {object:plural} to {goal}. The twist: {diversifier}.',
+      'A game where you {verb} {object:singular} on {setting:on}.',
+      'A game where you {verb} {object:singular} on {setting:on}. The twist: {diversifier}.',
+      'A game where the world is dominated by {supernatural:plural}, and the last hope for humanity is {person:singular} with {object:singular}.',
+      'A game where the world is dominated by {supernatural:plural}, and the last hope for humanity is {person:singular} with {object:singular}. To make matters worse, {diversifier}.',
+      'A game where {person:plural} {verb} {supernatural:plural} in {setting:in}.',
+      'A game where {person:plural} {verb} {supernatural:plural} in {setting:in}. The twist: {diversifier}.',
+      'A game where you feed {object:plural} to {supernatural:singular} in order to {goal}.',
+      'A game where you feed {object:plural} to {supernatural:singular} in order to {goal}. To make things more interesting, {diversifier}.',
+      'A game where you fight waves of {supernatural:plural} in {setting:in}.',
+      'A game where you fight waves of {supernatural:plural} in {setting:in}, but there\'s a catch: {diversifier}.',
+      'A game where {person:plural} compete with {supernatural:plural} in {setting:in}.',
+      'A game where {person:plural} compete with {supernatural:plural} in {setting:in}. The twist: {diversifier}.',
+      'A game about {animal:singular} with {object:plural} that will stop at nothing to {goal}.',
+      'A game about {animal:singular} with {object:plural} that will stop at nothing to {goal}. On top of that, {diversifier}.',
+      'A game about {person:singular} searching {setting:in} for their lost {object:singular}.',
+      'A game about {person:singular} searching {setting:in} for their lost {object:singular}. The twist: {diversifier}.',
+      'You are {person:singular} in {setting:in}. Armed only with {object:singular} and your wits, you must {goal}.',
+      'You are {person:singular} in {setting:in}. Armed only with {object:singular} and your wits, you must {goal}. To make matters worse, {diversifier}.',
+      'An {style:an} {genre:an} that takes places in {setting:in}.',
+      'An {style:an} {genre:an} that takes places in {setting:in}. The twist: {diversifier}.',
+      'A {style:a} {genre:a} where you {verb} {supernatural:singular} in {setting:in}.',
+      'A {style:a} {genre:a} where you {verb} {supernatural:singular} in {setting:in}, but {diversifier}.'
     ]
 
     return message.channel.send(generate(_.randomElement(templates)))
