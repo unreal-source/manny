ecd1199b59aca38de1ea3d0fc65a53844e2f785d
Nick Pfisterer <nick@polymoon.net>
2021-10-24 15:27:24 -0700

    Add command: Avatar

---
+++ src/commands/slash/utility/Avatar.js
@@ -0,0 +1,26 @@
+import { SlashCommand } from 'hiei.js'
+
+class Avatar extends SlashCommand {
+  constructor () {
+    super({
+      name: 'avatar',
+      description: 'View a large version of someone\'s avatar',
+      options: [
+        {
+          type: 'USER',
+          name: 'user',
+          description: 'The user who\'s avatar you want to see',
+          required: true
+        }
+      ]
+    })
+  }
+
+  async run (interaction) {
+    const user = interaction.options.getUser('user')
+
+    return interaction.reply({ content: user.displayAvatarURL({ size: 512 }) })
+  }
+}
+
+export default Avatar
