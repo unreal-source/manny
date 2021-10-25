c40da1c148941b95cc17d40e3fa5efbaedf1d948
Nick Pfisterer <nick@polymoon.net>
2021-10-24 17:18:54 -0700

    Add command: RollDice

---
+++ src/commands/slash/fun/RollDice.js
@@ -0,0 +1,52 @@
+import { SlashCommand } from 'hiei.js'
+import { randomInt } from '../../../utilities/Util.js'
+
+class RollDice extends SlashCommand {
+  constructor () {
+    super({
+      name: 'roll',
+      description: 'Roll up to 7 gaming dice',
+      options: [
+        {
+          type: 'INTEGER',
+          name: 'quantity',
+          description: 'The number of dice you want to roll.',
+          required: true,
+          choices: [
+            { name: '1', value: 1 },
+            { name: '2', value: 2 },
+            { name: '3', value: 3 },
+            { name: '4', value: 4 },
+            { name: '5', value: 5 },
+            { name: '6', value: 6 },
+            { name: '7', value: 7 }
+          ]
+        },
+        {
+          type: 'INTEGER',
+          name: 'sides',
+          description: 'The number of sides your dice should have.',
+          required: true,
+          choices: [
+            { name: 'd4', value: 4 },
+            { name: 'd6', value: 6 },
+            { name: 'd8', value: 8 },
+            { name: 'd10', value: 10 },
+            { name: 'd12', value: 12 },
+            { name: 'd20', value: 20 }
+          ]
+        }
+      ]
+    })
+  }
+
+  async run (interaction) {
+    const quantity = interaction.options.getInteger('quantity')
+    const sides = interaction.options.getInteger('sides')
+    const roll = Array(quantity).fill(0).map(i => randomInt(1, sides)).join(', ')
+
+    return interaction.reply({ content: `:game_die: ${roll}` })
+  }
+}
+
+export default RollDice
