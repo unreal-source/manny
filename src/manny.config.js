25fabf9cd63cb9609a7397208256f4a161ff1430
Nick Pfisterer <nick@polymoon.net>
2021-11-01 15:18:17 -0700

    Refactor bot config

---
+++ src/manny.config.js
@@ -0,0 +1,15 @@
+export const automod = {
+  newAccountAge: 30,
+  joinCount: 10,
+  automute: {
+    1: '10m',
+    2: '1h'
+  }
+}
+
+export const links = {
+  appeals: 'https://appeals.unrealslackers.org',
+  github: 'https://github.com/unrealslackers',
+  twitter: 'https://twitter.com/unrealslackers',
+  website: 'https://unrealslackers.org'
+}
