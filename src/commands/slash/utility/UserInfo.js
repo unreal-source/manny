e69812881e8cfc2879e43ba183da5d7c63c32044
Nick Pfisterer <nick@polymoon.net>
2021-11-03 14:58:03 -0700

    Add command: UserInfo

---
+++ src/commands/slash/utility/UserInfo.js
@@ -0,0 +1,44 @@
+import { SlashCommand } from 'hiei.js'
+import { MessageEmbed } from 'discord.js'
+import { time } from '@discordjs/builders'
+
+class UserInfo extends SlashCommand {
+  constructor () {
+    super({
+      name: 'user',
+      description: 'Learn more about a user',
+      options: [
+        {
+          type: 'USER',
+          name: 'user',
+          description: 'The user you want to learn about',
+          required: true
+        }
+      ]
+    })
+  }
+
+  async run (interaction) {
+    const member = interaction.options.getMember('user')
+    const status = {
+      online: 'Online',
+      idle: 'Idle',
+      dnd: 'Do Not Disturb',
+      offline: 'Offline'
+    }
+
+    const info = new MessageEmbed()
+      .setTitle(`${member.user.tag} ${member.nickname ? `(${member.nickname})` : ''} ${member.user.bot ? '`BOT`' : ''}`)
+      .setThumbnail(member.displayAvatarURL())
+      .addField('Status', status[member.presence.status], true)
+      .addField('ID', member.id, true)
+      .addField('Membership', member.pending ? 'Pending' : 'Confirmed')
+      .addField('Roles', member.roles.cache.map(role => `\`${role.name}\``).join(' '))
+      .addField('Joined Server', `${time(member.joinedAt)} • ${time(member.joinedAt, 'R')}`)
+      .addField('Joined Discord', `${time(member.user.createdAt)} • ${time(member.user.createdAt, 'R')}`)
+
+    return interaction.reply({ embeds: [info] })
+  }
+}
+
+export default UserInfo
