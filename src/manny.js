0643cb3d73bf15dc9684a38f28a3dfd330fc863a
Nick Pfisterer <nick@polymoon.net>
2021-11-03 14:58:14 -0700

    Add intent: GUILD_PRESENCES

--- src/manny.js
+++ src/manny.js
@@ -7,7 +7,8 @@ const client = new HieiClient({
   options: {
     intents: [
       Intents.FLAGS.GUILDS,
-      Intents.FLAGS.GUILD_MESSAGES
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_PRESENCES
     ]
   }
 })
