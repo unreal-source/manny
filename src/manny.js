aaa982dbd34af107f522b4d67527b40dba02b311
Nick Pfisterer <nick@polymoon.net>
2022-03-25 12:44:54 -0700

    :arrow_up: Upgrade to hiei.js 0.4.0

--- src/manny.js
+++ src/manny.js
@@ -2,15 +2,11 @@ import { Intents } from 'discord.js'
 import { HieiClient } from 'hiei.js'
 
 const client = new HieiClient({
-  commands: 'src/commands',
-  listeners: 'src/listeners',
-  options: {
-    intents: [
-      Intents.FLAGS.GUILDS,
-      Intents.FLAGS.GUILD_MESSAGES,
-      Intents.FLAGS.GUILD_PRESENCES
-    ]
-  }
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_PRESENCES
+  ]
 })
 
 client.login(process.env.TOKEN)
