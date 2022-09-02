205f440c221bfc3a15faf07982cab3eb849228a7
Nick Pfisterer <nick@polymoon.net>
2022-09-01 20:43:49 -0700

    Move FAQ choices to YAML file

--- src/commands/utility/faq.js
+++ src/commands/utility/faq.js
@@ -1,5 +1,7 @@
+import { readFile } from 'node:fs/promises'
 import { SlashCommand } from 'hiei.js'
 import { ApplicationCommandOptionType, PermissionFlagsBits } from 'discord.js'
+import yaml from 'js-yaml'
 
 class FAQ extends SlashCommand {
   constructor () {
@@ -19,18 +21,18 @@ class FAQ extends SlashCommand {
     })
   }
 
-  get choices () {
-    return [
-      { name: 'Will my 4.x project open in 5.0?', value: 'Upgrading your 4.x project to 5.0? See this knowledge base article to learn more about version compatibility when upgrading your project.\n<https://forums.unrealengine.com/docs?topic=503528>' },
-      { name: 'When does Epic release featured free Marketplace content?', value: 'Epic releases a new batch of featured free Marketplace content on the first Tuesday of each month.' }
-    ]
+  async choices () {
+    const file = await readFile('./src/commands/utility/faq.yml')
+    const data = await yaml.load(file)
+    return data
   }
 
   async run (interaction) {
     const question = interaction.options.getString('question')
-    const answer = this.choices.find(choice => choice.name === question).value
+    const choices = await this.choices()
+    const answer = choices.find(choice => choice.question === question).answer
 
-    return interaction.reply({ content: answer })
+    return interaction.reply({ content: `:question: **${question}**\n${answer}` })
   }
 }
 
