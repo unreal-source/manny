83c1c5d81e874b9d76797177c70dd68d7f9585e2
Nick Pfisterer <guardian.flame9641@fastmail.com>
2024-02-09 18:50:36 -0800

    Enable Prisma logging

--- src/utilities/prisma-client.js
+++ src/utilities/prisma-client.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error']
+})
 
 export default prisma
