d6ef49f016cc4fe23789c29bc87b3f8e6c586600
Nick Pfisterer <guardian.flame9641@fastmail.com>
2025-06-06 17:15:49 -0700

    Modify logger for Fastify v5 compatibility

--- src/utilities/logger.js
+++ src/utilities/logger.js
@@ -1,20 +1,25 @@
 import pino from 'pino'
-const localTransport = pino.transport({
+
+const localConfig = {
   target: 'pino-pretty',
   options: {
     translateTime: 'HH:MM:ss Z',
     ignore: 'pid,hostname'
   }
-})
+}
 
-const axiomTransport = pino.transport({
+const axiomConfig = {
   target: '@axiomhq/pino',
   options: {
     dataset: process.env.AXIOM_DATASET,
     token: process.env.AXIOM_TOKEN
   }
-})
+}
+
+const localTransport = pino.transport(localConfig)
+const axiomTransport = pino.transport(axiomConfig)
 
 const log = process.env.AXIOM_TOKEN ? pino(axiomTransport) : pino(localTransport)
+const fastifyOptions = process.env.AXIOM_TOKEN ? { transport: axiomConfig } : { transport: localConfig }
 
-export default log
+export { log, fastifyOptions }
