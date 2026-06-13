165febf5feed97892c2d8d6a071c1fb0aa3357a6
Nick Pfisterer <guardian.flame9641@fastmail.com>
2026-06-13 14:54:16 -0700

    Remove client error and warn events

--- src/events/evt.client-error.js
+++
@@ -1,9 +0,0 @@
-import { log } from '../utilities/logger.js'
-
-export default {
-  event: 'error',
-  emitter: 'client',
-  async execute (error) {
-    return log.error({ event: 'client-error', error })
-  }
-}
--- src/events/evt.client-warn.js
+++
@@ -1,9 +0,0 @@
-import { log } from '../utilities/logger.js'
-
-export default {
-  event: 'warn',
-  emitter: 'client',
-  async execute (info) {
-    return log.warn({ event: 'client-warn' }, info)
-  }
-}
