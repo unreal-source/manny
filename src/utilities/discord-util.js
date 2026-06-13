fc8c8cc1d06606ac08e05d2895c2553491a31709
Nick Pfisterer <guardian.flame9641@fastmail.com>
2026-06-13 15:20:48 -0700

    Add more Discord utilities

--- src/utilities/discord-util.js
+++ src/utilities/discord-util.js
@@ -12,3 +12,34 @@ export function createModalCollector (client, interaction) {
     max: 1
   })
 }
+
+/** Detect whether a member is an admin or moderator
+ * @param {member} member - The guild member
+*/
+export function isStaff (member) {
+  const isAdmin = member.roles.cache.some(role => role.id === Bun.env.ADMIN_ROLE)
+  const isModerator = member.roles.cache.some(role => role.id === Bun.env.MODERATOR_ROLE)
+
+  return isAdmin || isModerator
+}
+
+/** Detect whether a member has an old (Username#1234) or new (@username) username and return it in the correct format
+ * @param {member} member - The guild member
+*/
+export function getUsername (member) {
+  // Is this a guild member or a user?
+  if (member.user) {
+    // Have they claimed a new username?
+    if (member.user.tag.endsWith('#0')) {
+      return member.user.username
+    } else {
+      return member.user.tag
+    }
+  } else {
+    if (member.tag.endsWith('#0')) {
+      return member.username
+    } else {
+      return member.tag
+    }
+  }
+}
