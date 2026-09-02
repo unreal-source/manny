cc6f6b20f4b1824026e0c3e358594e2c96d7fb93
Nick Pfisterer <guardian.flame9641@fastmail.com>
2026-09-01 20:16:05 -0700

    Kick instead of timeout. Attempt to notify user.

--- src/listeners/message-create.js
+++ src/listeners/message-create.js
@@ -1,7 +1,6 @@
 import { EmbedBuilder } from 'discord.js'
 import { Listener } from 'hiei.js'
 import ms from 'ms'
-import { getUsername } from '../utilities/discord-util.js'
 import log from '../utilities/logger.js'
 import prisma from '../utilities/prisma-client.js'
 
@@ -37,9 +36,22 @@ class MessageCreate extends Listener {
     log.info(`${message.author.username} sent a suspicious message with ${attachmentCount} image attachments, ${urlCount} image URLs in ${distinctChannels.size} ${distinctChannels.size > 1 ? 'channels' : 'channel'}`)
 
     if (distinctChannels.size >= process.env.SUSPECT_CHANNEL_THRESHOLD) {
-      log.info(`Timing out ${message.author.username} for suspected account compromise`)
+      // We can't notify members after they leave the server, so we have to do it before kicking
       try {
-        await message.member.timeout(ms('1h'), 'Suspected account compromise')
+        const notification = new EmbedBuilder()
+          .setAuthor({ name: message.guild.name, iconURL: message.guild.iconURL() })
+          .setTitle('You were kicked from the server')
+          .setDescription(`**Reason:** Suspicious activity indicates your account may be compromised. For your safety, please change your password immediately and consider enabling two-factor authentication for added protection before you rejoin the server.`)
+          .setTimestamp()
+
+        await message.member.send({ embeds: [notification] })
+      } catch (error) {
+        log.error(`Failed to notify ${message.author.username}, not accepting direct messages`, error)
+      }
+
+      // Kick the suspect
+      try {
+        await message.member.kick('Suspected account compromise')
 
         for (const entry of entries) {
           await entry.message.delete()
@@ -47,21 +59,18 @@ class MessageCreate extends Listener {
 
         const incident = await prisma.case.create({
           data: {
-            action: 'Timed out',
-            member: getUsername(message.member),
+            action: 'Kicked',
+            member: message.member.username,
             memberId: message.member.id,
             moderator: this.client.user.username,
             moderatorId: this.client.user.id,
             reason: 'Suspected account compromise',
-            timeout: {
-              create: { duration: '1 hour' }
-            }
           }
         })
 
         const moderationLogChannel = message.guild.channels.cache.get(process.env.MODERATION_LOG_CHANNEL)
         const moderationLogEmbed = new EmbedBuilder()
-          .setAuthor({ name: '⏳ Timed out for 1 hour' })
+          .setAuthor({ name: '🥾 Kicked' })
           .setDescription(`**Member:** ${incident.member}\n**Member ID:** ${incident.memberId}\n**Reason:** ${incident.reason}`)
           .setFooter({ text: `Case ${incident.id} • ${incident.moderator}` })
           .setThumbnail(message.member.displayAvatarURL())
@@ -75,6 +84,8 @@ class MessageCreate extends Listener {
         })
 
         prisma.$disconnect()
+
+        log.info(`Kicked ${message.author.username} for suspected account compromise`)
       } catch (e) {
         log.error(e)
       }
