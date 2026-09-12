3d888694108df9c89fc757e4a49890ef9533f753
Nick Pfisterer <guardian.flame9641@fastmail.com>
2026-09-12 16:38:55 -0700

    Add member join/leave event listeners

---
+++ src/events/evt.member-leave.js
@@ -0,0 +1,19 @@
+import { ContainerBuilder, MessageFlags, SeparatorBuilder, SeparatorSpacingSize, TextDisplayBuilder, time } from 'discord.js'
+
+export default {
+  event: 'guildMemberRemove',
+  emitter: 'client',
+  async execute(member, client) {
+    const memberLog = await client.channels.fetch(process.env.MEMBER_LOG_CHANNEL)
+
+    return memberLog.send({
+      components: [
+        new ContainerBuilder()
+          .addTextDisplayComponents(new TextDisplayBuilder().setContent(`### :red_circle:   ${member.displayName} left the server`))
+          .addSeparatorComponents(new SeparatorBuilder().setSpacing(SeparatorSpacingSize.Small).setDivider(true))
+          .addTextDisplayComponents(new TextDisplayBuilder().setContent(`-# ${member.user.id} • Joined ${time(member.joinedAt, 'R')}`))
+      ],
+      flags: [MessageFlags.IsComponentsV2]
+    })
+  }
+}
