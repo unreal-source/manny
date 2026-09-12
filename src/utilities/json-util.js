03d7fc249ebfd157515aaa896aff12ea86854456
Nick Pfisterer <guardian.flame9641@fastmail.com>
2026-09-12 16:29:00 -0700

    Drop importJSON() utility, no longer needed as of Node.js 22

--- src/utilities/json-util.js
+++
@@ -1,8 +0,0 @@
-import { readFile } from 'node:fs/promises'
-
-/** Import and parse a JSON file.
- * @param {String} path - The path to the JSON file you want to import
-*/
-export async function importJson (path) {
-  return JSON.parse(await readFile(path))
-}
